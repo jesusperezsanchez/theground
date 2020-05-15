a91792d2c09bf337cc48e0565582f30e8d3e8d38
Jesus Perez Sanchez <32244603+jesusperezsanchez@users.noreply.github.com>
2020-05-15 21:22:09 +0200

    Update Thewall.sol

--- contracts/Thewall.sol
+++ contracts/Thewall.sol
@@ -1,1 +1,22 @@
+pragma solidity ^0.6.8;
 
+contract Thewall {
+    
+    address public manager;
+    address[] public  investors;
+    
+    constructor()  public {
+       manager = msg.sender; 
+    }
+    
+    function enter() public payable {
+       require(msg.value > .01 ether);      
+       investors.push(msg.sender);    
+    } 
+    
+       
+    function getInvestors() public view returns (address[] memory ) {
+        return investors;
+    }    
+        
+}
